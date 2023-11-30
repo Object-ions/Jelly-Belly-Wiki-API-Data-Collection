ec483cab07eb7789d54482d38b073e65f7e0924d
object-ions <moshikoatia@gmail.com>
2023-11-29 23:24:33 -0800

    Add condition to extract JSON file. script output limited only to 'Boba milk tea'

--- webScerer.py
+++ webScerer.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-def scrape_jelly_belly_flavor_details():
+def scrape_jelly_belly_flavor_details_to_json():
     # URL of the Jelly Belly Flavor Collections page
     url = 'https://www.jellybelly.com/jelly-belly-flavor-collections'
 
@@ -9,6 +10,9 @@ def scrape_jelly_belly_flavor_details():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
+
+    # Data structure to hold all flavors
+    all_flavor_data = []
 
     try:
         # Send GET request with a timeout and custom headers
@@ -21,15 +25,15 @@ def scrape_jelly_belly_flavor_details():
             
             # Find all <div> elements with class 'flavor-coll-details'
             flavor_details = soup.find_all('div', class_='flavor-coll-details')
-            
+
             if flavor_details:
                 for div in flavor_details:
                     # Extract the flavor group name
                     group_name = div.find('h2').get_text(strip=True)
-                    print(f"Group Name: {group_name}")
 
                     # Iterate over each flavor <li>
                     for li in div.find_all('li', {'aria-label': 'bean'}):
+                        flavor_info = {}
                         try:
                             # Extract background color
                             style = li.get('style')
@@ -42,13 +46,24 @@ def scrape_jelly_belly_flavor_details():
                             # Extract flavor name
                             flavor_name = li.find('p', class_='blue-text').get_text(strip=True) if li.find('p', class_='blue-text') else 'No Flavor Name'
 
-                            print(f"\tFlavor Name: {flavor_name}")
-                            print(f"\tBackground Color: {background_color}")
-                            print(f"\tImage URL: {image_url}\n")
+                            # Add flavor info to the list
+                            flavor_info = {
+                                'Group Name': group_name,
+                                'Flavor Name': flavor_name,
+                                'Background Color': background_color,
+                                'Image URL': image_url
+                            }
+                            all_flavor_data.append(flavor_info)
+
                         except IndexError as e:
                             print("\tAn error occurred while processing a flavor item:", e)
             else:
                 print("Flavor details section not found")
+
+            # Write data to JSON file
+            with open('jelly_belly_flavors.json', 'w', encoding='utf-8') as json_file:
+                json.dump(all_flavor_data, json_file, indent=4, ensure_ascii=False)
+
         else:
             print(f"Failed to retrieve the page. Status Code: {response.status_code}")
     
@@ -56,4 +71,4 @@ def scrape_jelly_belly_flavor_details():
         print(f"An error occurred: {e}")
 
 # Run the function
-scrape_jelly_belly_flavor_details()
+scrape_jelly_belly_flavor_details_to_json()
