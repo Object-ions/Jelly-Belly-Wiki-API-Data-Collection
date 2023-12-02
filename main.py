c1e650be3069449a2e2ddf04e4a4ca0c89bbdf91
object-ions <moshikoatia@gmail.com>
2023-12-02 13:08:43 -0800

    Update script. not working. WIP

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
-from time import sleep
-driver = webdriver.Chrome()
+chrome_path = "/usr/local/bin/chromedriver"
+
+driver = webdriver.Chrome(chrome_path)
+
 driver.get("https://www.jellybelly.com/jelly-belly-single-flavors")
+driver.find_element_by_xpath("""//*[@id="7up"]""").click()
 
-
-print("hello world")
-sleep(3)
+posts = driver.find_elements_by_xpath("""//*[@id="flavor-content"]/div""")
+for post in posts:
+  print(post.text)
