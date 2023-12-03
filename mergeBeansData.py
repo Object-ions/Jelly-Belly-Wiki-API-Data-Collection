d895b5561084176fdee7b3b10364da3f07af1c44
object-ions <moshikoatia@gmail.com>
2023-12-03 12:30:27 -0800

    Update script to merge new files

--- mergeBeansData.py
+++ mergeBeansData.py
@@ -1,5 +1,9 @@
-# This script will take the 2 JSON files and cross information and merge duplicates between the files.
-    # Add bools and BeanId in a sequence
+# This script will:
+
+# Load and normalize the data from 'static_data.json' and 'dynamic_data.json'.
+# Merge the data based on the FlavorName.
+# Keep track of and display items from both datasets that do not find a match.
+# Save the merged data to 'merged_beans_data.json'.
 
 import json
 
@@ -8,39 +12,52 @@ def load_json(filename):
     with open(filename, 'r') as file:
         return json.load(file)
 
+# Function to save JSON data to a file
+def save_json(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+# Normalize a string (e.g., FlavorName)
+def normalize(name):
+    return name.strip().upper()
+
 # Load data from both JSON files
-jelly_belly_flavors = load_json('jelly_belly_flavors.json')
-main_results = load_json('main_results_capitalized.json')
+static_data = load_json('static_data.json')
+dynamic_data = load_json('dynamic_data.json')
 
-# Normalize the FlavorName to uppercase in both datasets for consistency
-for item in jelly_belly_flavors:
-    item['FlavorName'] = item['FlavorName'].upper()
+# Normalize the FlavorName in both datasets
+for item in static_data:
+    item['FlavorName'] = normalize(item['FlavorName'])
 
-for item in main_results:
-    item['FlavorName'] = item['FlavorName'].upper()
+for item in dynamic_data:
+    item['FlavorName'] = normalize(item['FlavorName'])
 
-# Merge data based on FlavorName
+# Merge data and track unmatched items
 merged_data = []
-bean_id = 1
+unmatched_static = []
+unmatched_dynamic = {item['FlavorName']: item for item in dynamic_data}
 
-for jelly_item in jelly_belly_flavors:
-    flavor_name = jelly_item['FlavorName']
-    # Find the matching item in main_results
-    main_item = next((item for item in main_results if item['FlavorName'] == flavor_name), None)
-    
-    if main_item:
-        # Merge the two items, with priority to main_item's fields
-        merged_item = {**jelly_item, **main_item}
-        # Add additional fields
-        merged_item['BeanId'] = bean_id
+for static_item in static_data:
+    flavor_name = static_item['FlavorName']
+    dynamic_item = unmatched_dynamic.pop(flavor_name, None)
+
+    if dynamic_item:
+        merged_item = {**static_item, **dynamic_item}
         merged_item['GlutenFree'] = False
         merged_item['SugarFree'] = False
         merged_item['Seasonal'] = False
         merged_item['Kosher'] = False
-
         merged_data.append(merged_item)
-        bean_id += 1
+    else:
+        unmatched_static.append(static_item)
+
+# Save unmatched items to separate JSON files
+save_json(unmatched_static, 'unmatched_static_data.json')
+save_json(list(unmatched_dynamic.values()), 'unmatched_dynamic_data.json')
 
 # Save the merged data to a new JSON file
-with open('merged_beans_data.json', 'w') as file:
-    json.dump(merged_data, file, indent=4)
+save_json(merged_data, 'merged_beans_data.json')
+
+print("Merged data saved to 'merged_beans_data.json'")
+print("Unmatched static data saved to 'unmatched_static_data.json'")
+print("Unmatched dynamic data saved to 'unmatched_dynamic_data.json'")
