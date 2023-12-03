43de0396affa73a362a84aa5bc18ecd44e522b21
object-ions <moshikoatia@gmail.com>
2023-12-02 20:27:19 -0800

    Update the code to append new result into the array (manually change the xpath og the clicked element)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import json
+import os
 
 # Initialize Chrome Options
 chrome_options = webdriver.ChromeOptions()
@@ -13,20 +14,26 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Navigate to the website and interact with it
 driver.get("https://www.jellybelly.com/jelly-belly-single-flavors")
-button = driver.find_element(By.XPATH, """//*[@id="7up"]/button""")
+button = driver.find_element(By.XPATH, """//*[@id="a-w-cream-soda"]/button""")
 button.click()
 
 # Find elements and extract their text
 description_elements = driver.find_elements(By.XPATH, """//*[@id="descriptionContainer"]""")
 extracted_data = []
 
+# Check if the file exists and load existing data
+if os.path.exists("main_results.json"):
+    with open("main_results.json", "r") as file:
+        extracted_data = json.load(file)
+
+# Extract and append new data
 for element in description_elements:
     text_parts = element.text.split('\n')
     
     # Extract relevant parts based on their position
-    flavor_name = text_parts[1]  # 2nd result
-    description = text_parts[2]  # 3rd result
-    ingredients = text_parts[6]  # 7th result
+    flavor_name = text_parts[1]
+    description = text_parts[2]
+    ingredients = text_parts[6]
 
     # Append to list as dictionary
     extracted_data.append({
