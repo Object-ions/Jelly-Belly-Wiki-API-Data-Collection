efd226255c51f83b6a325b80fa1d16c08becc637
object-ions <moshikoatia@gmail.com>
2023-12-03 00:03:15 -0800

    Finalize script - find missing names from joined lists

---
+++ searchForMissingNames.py
@@ -0,0 +1,57 @@
+# To accomplish the task of comparing the list of flavors from 'beans_name_list.json' with the entries in 'merged_98_beans.json', and identifying any missing flavors, here's the approach I'll take:
+
+# Read Data from JSON Files: I'll read the contents of 'beans_name_list.json' and 'merged_98_beans.json'. The first file will provide a list of all expected flavor names, and the second file will contain the actual entries.
+
+# Normalize Flavor Names: Since there's a naming difference (e.g., "GlazedBlueberryCake" vs. "GLAZED BLUEBERRY CAKE"), I need to normalize these names to ensure accurate comparison. This will likely involve converting all names to a common case (e.g., lowercase) and removing non-alphanumeric characters (like spaces and punctuation).
+
+# Compare Flavors: With normalized names, I'll compare the list of expected flavors against the actual flavors in 'merged_98_beans.json'. I'll check which names from 'beans_name_list.json' are not present in 'merged_98_beans.json'.
+
+# Output Missing Flavors: Finally, I'll output the list of missing flavor names to the terminal.
+
+# This approach ensures that despite the differences in naming conventions, the comparison will be accurate, and you'll get a list of any flavors that are present in 'beans_name_list.json' but missing in 'merged_98_beans.json'.
+
+import json
+
+# Function to read JSON file
+def read_json(file_name):
+    with open(file_name, 'r') as file:
+        return json.load(file)
+
+# Normalize flavor names for comparison
+def normalize_name(name):
+    return ''.join(filter(str.isalnum, name)).lower()
+
+# Read data from JSON files
+flavor_list = read_json('beans_name_list.json')
+merged_beans = read_json('merged_98_beans.json')
+
+# Extract and normalize flavor names from merged_beans
+merged_flavors = set(normalize_name(bean['FlavorName']) for bean in merged_beans)
+
+# Check for missing flavors
+missing_flavors = []
+for flavor in flavor_list:
+    if normalize_name(flavor) not in merged_flavors:
+        missing_flavors.append(flavor)
+
+# Display missing flavors
+print("Missing Flavors:")
+for flavor in missing_flavors:
+    print(flavor)
+
+# Optionally, you can also write the missing flavors to a file
+# with open('missing_flavors.json', 'w') as file:
+#     json.dump(missing_flavors, file, indent=4)
+
+
+
+# ** results **
+
+# a-w-root-bear
+# Apple-Pie-la-Cold-Stone
+# BerryBlue
+# BirthdayCake
+# BirthdayCakeRemix
+# BlueRaspberry
+# PinaColada
+# SourApple
